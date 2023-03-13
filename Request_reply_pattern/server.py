2d6a75ef8a3c05552302332bed9e2b3fc521142f
prithviBytes <prithvisuvarna56@gmail.com>
2023-03-13 17:07:58 +0530

    add: request-reply's server config

--- Request_reply_pattern/server.py
+++ Request_reply_pattern/server.py
@@ -1,26 +1,26 @@
 import pika
 from pika.exchange_type import ExchangeType
 
-def on_message_received(ch, method, properties, body):
-    print(f"First_consumer has received the message")
+def on_request_message_received(ch, method, properties, body):
+    print(f"Received request: {properties.correlation_id}")
+    ch.basic_publish(
+        "",
+        routing_key=properties.reply_to,
+        body=f"This is response to {properties.correlation_id}"
+    )
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', port=5672))
 channel = connection.channel()
 
-exchange_name = "pubsub"
-channel.exchange_declare(exchange=exchange_name, exchange_type=ExchangeType.fanout)
+request_queue_name = "request-queue"
 
-current_queue = channel.queue_declare(queue='', exclusive=True)
+channel.queue_declare(queue=request_queue_name)
 
-channel.queue_bind(
-    exchange=exchange_name, 
-    queue=current_queue.method.queue
-)
 
 channel.basic_consume(
-    queue=current_queue.method.queue,
-    on_message_callback=on_message_received
+    queue=request_queue_name,
+    on_message_callback=on_request_message_received
 )
 
-print('Waiting for messages...')
+print('Server Waiting for messages...')
 channel.start_consuming()
